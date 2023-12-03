fa79aabfeecdfd74220e88a941df279aeb6b7ac6
wookki <bwlimtony@gmail.com>
2023-12-04 01:52:08 +0900

    feat: react-hook-form

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,29 @@
-import Layout from '@/components/Layout'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { SessionProvider } from 'next-auth/react'
-import { RecoilRoot } from 'recoil'
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 
-const queryClient = new QueryClient()
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { session } = pageProps
+  const { session } = pageProps;
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <SessionProvider session={session}>
           <Layout>
             <Component {...pageProps} />
+            <ToastContainer />
           </Layout>
           <ReactQueryDevtools />
         </SessionProvider>
       </QueryClientProvider>
     </RecoilRoot>
-  )
-} 
+  );
+}
