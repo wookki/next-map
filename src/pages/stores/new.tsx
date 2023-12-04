b7e0df42700e5ada12705ff1b4d65a38c262a6b9
wookki <bwlimtony@gmail.com>
2023-12-04 15:52:29 +0900

    feat: 맛집 삭제하기 기능 추가

--- src/pages/stores/new.tsx
+++ src/pages/stores/new.tsx
@@ -18,11 +18,8 @@ export default function StoreNewPage() {
     <form
       className="px-4 md:max-w-4xl mx-auto py-8"
       onSubmit={handleSubmit(async (data) => {
-        console.log(data);
         try {
           const result = await axios.post("/api/stores", data);
-          console.log(result);
-
           if (result.status === 200) {
             // 성공 케이스
             toast.success("맛집을 등록했습니다.");
@@ -114,9 +111,11 @@ export default function StoreNewPage() {
                 )}
               </div>
             </div>
-
-            <AddressSearch setValue={setValue} register={register} errors={errors} />
-
+            <AddressSearch
+              setValue={setValue}
+              register={register}
+              errors={errors}
+            />
             <div className="sm:col-span-2 sm:col-start-1">
               <label
                 htmlFor="city"
@@ -156,7 +155,7 @@ export default function StoreNewPage() {
                   {...register("storeType", { required: true })}
                   className="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 >
-                  <option value="">식품인증구분 선택</option>
+                  <option value="">업종구분 선택</option>
                   {STORE_TYPE_ARR?.map((data) => (
                     <option key={data} value={data}>
                       {data}
@@ -177,6 +176,7 @@ export default function StoreNewPage() {
       <div className="mt-6 flex items-center justify-end gap-x-6">
         <button
           type="button"
+          onClick={() => router.back()}
           className="text-sm font-semibold leading-6 text-gray-900"
         >
           뒤로가기
